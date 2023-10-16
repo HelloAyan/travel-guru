f89e934681c94669aaf1e25e4c0efc304348e979
helloayan <18103373@iubat.edu>
2023-10-16 17:34:21 +0600

    Setup firebase and changed background image

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEEy5fDwpqtzIAD_x17aHJRxM7RY89kYI",
+    authDomain: "travel-guru-322b3.firebaseapp.com",
+    projectId: "travel-guru-322b3",
+    storageBucket: "travel-guru-322b3.appspot.com",
+    messagingSenderId: "330266810191",
+    appId: "1:330266810191:web:dcc5e1f3ba455aa180d73a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
