95b6c0d4d401d9f07231b9807a00bdd1a91a44eb
helloayan <18103373@iubat.edu>
2023-10-20 23:04:37 +0600

    designed header part

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Main.module.css';
 import { Container } from 'react-bootstrap';
+import Header from '../pages/Shared/Header/Header';
 
 const Main = () => {
     return (
-        <Container fluid className={styles.my_container}>
-            <h2>hello</h2>
+        <Container fluid className={`${styles.my_container} `} >
+            <Header></Header>
         </Container>
     )
 }
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../../assets/logo.png'
+import { Navbar, Container, Nav, NavDropdown, Form, Button } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <Navbar expand="lg" className="">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img className='img-responsive' src={logo} alt="Logo " width={120} height={56} />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-between' >
+                    <Nav style={{ width: '320px' }}>
+                        <Form.Control
+                            type="search"
+                            placeholder="Search Your Destination"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                    </Nav>
+
+                    <Nav className="d-flex align-item-center">
+                        <Nav.Link href="#home">News</Nav.Link>
+                        <Nav.Link href="#link">Destination</Nav.Link>
+                        <Nav.Link href="#link">Blog</Nav.Link>
+                        <Nav.Link href="#link">Contact</Nav.Link>
+                        <Nav.Link href="#link" className='bg-warning ps-5'>
+                            Login
+                        </Nav.Link>
+
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
