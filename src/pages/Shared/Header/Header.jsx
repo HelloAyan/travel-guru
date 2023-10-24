c402078f69014cb62d9a699801499dd43f7ec134
helloayan <18103373@iubat.edu>
2023-10-24 18:15:03 +0600

    modified header section

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Navbar, Container, Nav, NavDropdown, Form, Button } from 'react-bootstr
 
 const Header = () => {
     return (
-        <Navbar expand="lg" className="">
+        <Navbar expand="lg">
             <Container>
                 <Navbar.Brand href="#home">
                     <img className='img-responsive' src={logo} alt="Logo " width={120} height={56} />
@@ -20,14 +20,12 @@ const Header = () => {
                         />
                     </Nav>
 
-                    <Nav className="d-flex align-item-center">
-                        <Nav.Link href="#home">News</Nav.Link>
-                        <Nav.Link href="#link">Destination</Nav.Link>
-                        <Nav.Link href="#link">Blog</Nav.Link>
-                        <Nav.Link href="#link">Contact</Nav.Link>
-                        <Nav.Link href="#link" className='bg-warning px-4 fw-semibold'>
-                            Login
-                        </Nav.Link>
+                    <Nav className="d-flex justify-content-center align-item-center fs-5">
+                        <div className='px-3'>News</div>
+                        <div className='px-3'>Destination</div>
+                        <div className='px-3'>Blog</div>
+                        <div className='px-3'>Contact</div>
+                        <Button variant="warning fs-6">Login</Button>
 
                     </Nav>
 
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { FaArrowRight } from 'react-icons/fa';
 
 const LeftNav = () => {
     return (
         <Container className='pt-5'>
             <h1>Cox's bazar</h1>
             <p>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
-            <Button variant="warning">Warning</Button>
+            <Button variant="warning">Booking <FaArrowRight style={{ color: 'black' }}></FaArrowRight></Button>
         </Container>
     )
 }
